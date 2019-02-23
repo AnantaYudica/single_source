d4f44ab6d5b7c0a4caa2888c1d2e9f5a8d829699
Ananta Yudica <nta040689@gmail.com>
2019-02-23 09:10:44 +0700

    Added memory management Added static constant values Added extfile position and extname output Added equal and not-eq condition Added check file or dir Added check undefined pathname

--- include/Pathname.h
+++ include/Pathname.h
@@ -2,6 +2,7 @@
 #define PATHNAME_H_
 
 #include <string>
+#include <memory>
 #include <regex>
 
 #include "Interval.h"
@@ -9,17 +10,27 @@
 class Pathname
 {
 public:
+    typedef std::shared_ptr<Pathname> PointerType;
+    typedef std::weak_ptr<Pathname> WeakPointerType;
     typedef std::string StringType;
     typedef Interval<typename StringType::const_iterator> IntervalType;
 private:
-    static const std::regex ms_pathname_regex;
+    static const std::string ms_norm_conj_dir_str;
+    static const std::string ms_parent_dir_str;
+    static const std::regex ms_rep_conj_dir_regex;
+    static const std::regex ms_dir_filename_regex;
+    static const std::regex ms_file_extname_regex;
+    static const std::regex ms_search_dir_regex;
 private:
     IntervalType m_dirname_strpos;
     IntervalType m_filename_strpos;
+    IntervalType m_extname_strpos;
     StringType m_pathname_str;
 private:
-    static void DirFilename(const std::string & pathname,
-        IntervalType & dirname_strpos, IntervalType & filename_strpos);
+    static void Normalize(std::string & pathname);
+    static void DirFileExtname(const std::string & pathname,
+        IntervalType & dirname_strpos, IntervalType & filename_strpos,
+        IntervalType & extname_strpos);
 public:
     Pathname();
     Pathname(std::string pathname_str);
@@ -30,10 +41,18 @@ public:
     Pathname & operator=(const Pathname & cpy);
     Pathname & operator=(Pathname && mov);
 public:
+    bool operator==(const Pathname & pathname) const;
+    bool operator!=(const Pathname & pathname) const;
+public:
     std::string GetPathname() const;
     std::string GetDirname() const;
     std::string GetFilename() const;
-
+    std::string GetExtname() const;
+public:
+    bool IsFile() const;
+    bool IsDirectory() const;
+public:
+    operator bool() const;
 };
 
 #endif //!_PATHNAME_H_
