bf14c43c77e5317bb777f46b27da337a2512cbdc
Ananta Yudica <nta040689@gmail.com>
2019-02-25 10:02:19 +0700

    Added class singleton for storage configure value.

---
+++ include/Configure.h
@@ -0,0 +1,41 @@
+#ifndef CONFIGURE_H_
+#define CONFIGURE_H_
+
+#include <cstddef>
+
+#include "cfg/Setting.h"
+
+class Configure final
+{
+private:
+    cfg::Setting m_value_setting;
+    cfg::Setting m_minimum_setting;
+    cfg::Setting m_maximum_setting;
+private:
+    static Configure ms_instance_cfg;
+public:
+    static Configure & GetInstance();
+private:
+    Configure();
+public:
+    Configure(const Configure &) = delete;
+    Configure(Configure &&) = delete;
+public:
+    Configure & operator=(const Configure &) = delete;
+    Configure & operator=(Configure &&) = delete;
+private:
+    template<typename TValue>
+    bool Validate(const TValue & value, const TValue & min,
+        const TValue & max);
+public:
+    void SetMinimum(const cfg::Setting & setting);
+    void SetMaximum(const cfg::Setting & setting);
+    void Set(const cfg::Setting & setting);
+public:
+    bool EnableThread() const;
+public:
+    std::size_t GetThreadSize() const;
+    std::size_t GetStringBufferSize() const;
+};
+
+#endif //!CONFIGURE_H_
---
+++ src/Configure.cpp
@@ -0,0 +1,59 @@
+#include "Configure.h"
+
+using namespace std;
+
+Configure Configure::ms_instance_cfg;
+
+Configure & Configure::GetInstance()
+{
+    return ms_instance_cfg;
+}
+
+Configure::Configure()
+{}
+
+template<typename TValue>
+bool Configure::Validate(const TValue & value, const TValue & min,
+    const TValue & max)
+{
+    return min == max || (value >= min && value <= max);
+}
+
+void Configure::SetMinimum(const cfg::Setting & setting)
+{
+    m_minimum_setting.ThreadSize = setting.ThreadSize;
+    m_minimum_setting.StringBufferSize = setting.StringBufferSize;
+}
+
+void Configure::SetMaximum(const cfg::Setting & setting)
+{
+    m_maximum_setting.ThreadSize = setting.ThreadSize;
+    m_maximum_setting.StringBufferSize = setting.StringBufferSize;
+}
+
+void Configure::Set(const cfg::Setting & setting)
+{
+    m_value_setting.EnableThread = setting.EnableThread;
+
+    if (Validate(setting.ThreadSize, m_minimum_setting.ThreadSize,
+        m_maximum_setting.ThreadSize))
+            m_value_setting.ThreadSize = setting.ThreadSize;
+    if (Validate(setting.StringBufferSize, m_minimum_setting.StringBufferSize,
+        m_maximum_setting.StringBufferSize))
+            m_value_setting.StringBufferSize = setting.StringBufferSize;
+}
+
+bool Configure::EnableThread() const
+{
+    return m_value_setting.EnableThread;
+}
+
+std::size_t Configure::GetThreadSize() const
+{
+    return m_value_setting.ThreadSize;
+}
+
+std::size_t Configure::GetStringBufferSize() const
+{
+    return m_value_setting.StringBufferSize;
+}
