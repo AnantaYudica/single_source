3446d82a42ddded7ebf7cc2270cde0e1961f412f
Ananta Yudica <nta040689@gmail.com>
2019-03-14 07:42:34 +0700

    Added declaration class avl interface Node

---
+++ include/struc/tree/avl/intf/Node.decl.h
@@ -0,0 +1,24 @@
+#ifndef STRUC_TREE_AVL_INTF_NODE_DECL_H_
+#define STRUC_TREE_AVL_INTF_NODE_DECL_H_
+
+namespace struc
+{
+namespace tree
+{
+namespace avl
+{
+namespace intf
+{
+
+template<typename TData>
+class Node;
+
+} //!intf
+
+} //!avl
+
+} //!tree
+
+} //!struc
+
+#endif //!STRUC_TREE_AVL_INTF_NODE_DECL_H_
