161b24311d1a467b75ef42bbbecedc959e09e8ca
Ananta Yudica <nta040689@gmail.com>
2019-03-28 18:42:07 +0700

    moved declare enum class Way from struc::tree::avl::imp::file::Pointer

---
+++ include/struc/tree/avl/defn/ptr/Way.h
@@ -0,0 +1,33 @@
+#ifndef STRUC_TREE_AVL_DEFN_PTR_WAY_H_
+#define STRUC_TREE_AVL_DEFN_PTR_WAY_H_
+
+namespace struc
+{
+namespace tree
+{
+namespace avl
+{
+namespace defn
+{
+namespace ptr
+{
+
+enum class Way
+{
+    undefined,
+    parent,
+    right,
+    left
+};
+
+} //!ptr
+
+} //!defn
+
+} //!avl
+
+} //!tree
+
+} //!struc
+
+#endif //!STRUC_TREE_AVL_DEFN_PTR_WAY_H_
