075e5cb75e0f387fd757cfdbcfae75a62fb9de15
Ananta Yudica <nta040689@gmail.com>
2019-04-03 00:47:26 +0700

    Fixed dependencies

--- include/intf/file/Output.h
+++ include/intf/file/Output.h
@@ -1,11 +1,11 @@
 #ifndef INTF_FILE_OUTPUT_H_
 #define INTF_FILE_OUTPUT_H_
 
-#include "../defn/file/Mode.h"
-#include "../defn/file/Offset.h"
-#include "../defn/file/Position.h"
-#include "../defn/file/Size.h"
-#include "../defn/file/Way.h"
+#include "../../defn/file/Mode.h"
+#include "../../defn/file/Offset.h"
+#include "../../defn/file/Position.h"
+#include "../../defn/file/Size.h"
+#include "../../defn/file/Way.h"
 
 namespace intf
 {
@@ -29,10 +29,9 @@ public:
     virtual bool IsOpen() = 0;
 private:
     virtual PositionType SeekPosition(const PositionType & pos,
-        const ModeValueType & mode = defn::file::mode::DefaultValue) = 0; 
+        const ModeValueType & mode) = 0; 
     virtual PositionType SeekOffset(const OffsetType & off,
-        const WayType & way, const ModeValueType & mode = 
-        defn::file::mode::DefaultValue) = 0;
+        const WayType & way, const ModeValueType & mode) = 0;
 public:
     PositionType SeekPosition(const PositionType & pos); 
     PositionType SeekOffset(const OffsetType & off,
