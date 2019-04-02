14019fe63090ebde4f87cee1b4c17df2821cbe80
Ananta Yudica <nta040689@gmail.com>
2019-04-03 00:45:59 +0700

    Updated element name enum Updated type definition

--- include/defn/rec/Status.h
+++ include/defn/rec/Status.h
@@ -1,18 +1,20 @@
 #ifndef DEFN_REC_STATUS_H_
 #define DEFN_REC_STATUS_H_
 
+#include <cstdint>
+
 namespace defn
 {
 namespace rec
 {
 
-typedef char StatusValueType;
+typedef std::uint8_t StatusValueType;
 
 enum class Status : StatusValueType
 {
     initial = 0x01,
     good = 0x02,
-    out_sync = 0x04,
+    out_of_sync = 0x04,
     bad = 0x80,
 };
 
