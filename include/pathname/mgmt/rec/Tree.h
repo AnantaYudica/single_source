073f0bc6eb8465b212df722af28357d01caf703a
Ananta Yudica <nta040689@gmail.com>
2019-04-03 00:44:32 +0700

    Updated implementation base class

--- include/pathname/mgmt/rec/Tree.h
+++ include/pathname/mgmt/rec/Tree.h
@@ -1,7 +1,13 @@
 #ifndef PATHNAME_MGMT_REC_TREE_H_
 #define PATHNAME_MGMT_REC_TREE_H_
 
+#include "../../../defn/rec/Size.h"
+#include "../../../intf/Record.h"
+#include "../../../intf/File.h"
+
 #include <cstdint>
+#include <cstddef>
+#include <string>
 
 namespace pathname
 {
@@ -10,13 +16,39 @@ namespace mgmt
 namespace rec
 {
 
-class Tree
+class Tree :
+    public ::intf::Record
 {
+public:
+    typedef intf::Record RecordInterfaceType;
+public:
+    typedef typename RecordInterfaceType::OffsetType OffsetType;
+    typedef typename RecordInterfaceType::PositionType PositionType;
+    typedef typename RecordInterfaceType::SizeType SizeType;
+    typedef typename RecordInterfaceType::WayType WayType;
+public:
+    typedef typename RecordInterfaceType::InputType InputType;
+    typedef typename RecordInterfaceType::OutputType OutputType;
+public:
+    typedef typename RecordInterfaceType::StatusValueType StatusValueType;
+    typedef typename RecordInterfaceType::StatusType StatusType;
+public:
+    typedef intf::File FileInterfaceType;
+    typedef typename FileInterfaceType::PositionType FilePositionType;
 private:
-    std::int64_t m_position;
+    typedef std::uint8_t SyncType;
+private:
+    static constexpr SyncType ms_position_sync = 0x01;
+public:
+    static constexpr std::size_t Size();
+private:
+    SyncType m_sync_flags;
+    FilePositionType m_position;
+    std::string m_pathname;
 public:
     Tree();
-    Tree(const std::int64_t & pos);
+    Tree(const FilePositionType & pos);
+    Tree(const std::string & pathname);
 public:
     Tree(const Tree & cpy);
     Tree(Tree && mov);
@@ -24,16 +56,36 @@ public:
     Tree & operator=(const Tree & cpy);
     Tree & operator=(Tree && mov);
 public:
-    Tree & operator=(const std::int64_t & pos);
+    FilePositionType Position() const;
+    void Position(const FilePositionType & pos);
 public:
-    std::int64_t & operator*();
-    const std::int64_t & operator*() const;
+    std::string Pathname() const;
+    void Pathname(const std::string & pathname);
+public:
+    SizeType Put(OutputType & out) const;
+public:
+    SizeType Get(InputType & in);
+public:
+    bool operator==(const RecordInterfaceType & rec) const;
+    bool operator!=(const RecordInterfaceType & rec) const;
 };
+
+constexpr std::size_t Tree::Size()
+{
+    return sizeof(FilePositionType);
+}
 
 } //rec
 
 } //!mgmt
 
 } //!pathname
+
+template<>
+struct defn::rec::Size<pathname::mgmt::rec::Tree>
+{
+    typedef std::size_t ValueType;
+    static constexpr ValueType Value = pathname::mgmt::rec::Tree::Size();
+};
 
 #endif //!PATHNAME_MGMT_REC_TREE_H_
--- src/pathname/mgmt/rec/Tree.cpp
+++ src/pathname/mgmt/rec/Tree.cpp
@@ -5,50 +5,129 @@
 using namespace std;
 using namespace pathname::mgmt::rec;
 
-
 Tree::Tree() :
-    m_position(-1)
+    ::intf::Record(),
+    m_sync_flags(0),
+    m_position(-1),
+    m_pathname()
 {}
 
-Tree::Tree(const std::int64_t & pos) :
-    m_position(pos)
+Tree::Tree(const FilePositionType & pos) :
+    ::intf::Record(),
+    m_sync_flags(0),
+    m_position(pos),
+    m_pathname()
+{}
+
+Tree::Tree(const std::string & pathname) :
+    ::intf::Record(),
+    m_sync_flags(0),
+    m_position(-1),
+    m_pathname(pathname)
 {}
 
 Tree::Tree(const Tree & cpy) :
-    m_position(cpy.m_position)
+    ::intf::Record(cpy),
+    m_sync_flags(cpy.m_sync_flags),
+    m_position(cpy.m_position),
+    m_pathname(cpy.m_pathname)
 {}
 
 Tree::Tree(Tree && mov) :
-    m_position(mov.m_position)
+    ::intf::Record(move(mov)),
+    m_sync_flags(mov.m_sync_flags),
+    m_position(mov.m_position),
+    m_pathname(move(mov.m_pathname))
 {
+    mov.m_sync_flags = 0;
     mov.m_position = -1;
+    mov.m_pathname.clear();
 }
 
 Tree & Tree::operator=(const Tree & cpy)
 {
+    ::intf::Record::operator=(cpy);
+    m_sync_flags = cpy.m_sync_flags;
     m_position = cpy.m_position;
+    m_pathname = cpy.m_pathname;
     return *this;
 }
 
 Tree & Tree::operator=(Tree && mov)
 {
+    ::intf::Record::operator=(move(mov));
+    m_sync_flags = mov.m_sync_flags;
     m_position = mov.m_position;
+    m_pathname = move(mov.m_pathname);
+    mov.m_sync_flags = 0;
     mov.m_position = -1;
+    mov.m_pathname.clear();
     return *this;
 }
 
-Tree & Tree::operator=(const std::int64_t & pos)
+typename Tree::FilePositionType Tree::Position() const
 {
+    return m_position;
+}
+
+void Tree::Position(const FilePositionType & pos)
+{
+    if (m_position == pos)return;
     m_position = pos;
-    return *this;
+    m_sync_flags |= ms_position_sync;
+    OutOfSynchronization(*this);
 }
 
-std::int64_t & Tree::operator*()
+string Tree::Pathname() const
 {
-    return m_position;
+    return m_pathname;
 }
 
-const std::int64_t & Tree::operator*() const
+void Tree::Pathname(const string & pathname)
 {
-    return m_position;
+    if (m_pathname.compare(pathname) == 0) return;
+    m_pathname = pathname;
+}
+
+typename Tree::SizeType Tree::Put(OutputType & out) const
+{
+    if (m_sync_flags & ms_position_sync)
+    {
+        if (!out.Put((const char *)&m_position, sizeof(FilePositionType)))
+            return Bad<SizeType>(*this, 0);
+        const_cast<SyncType &>(m_sync_flags) ^= ms_position_sync;
+    }
+    else
+        out.SeekOffset(sizeof(FilePositionType), WayType::current);
+    return Good<SizeType>(*this, sizeof(FilePositionType));
+}
+
+typename Tree::SizeType Tree::Get(InputType & in)
+{
+    if (m_sync_flags & ms_position_sync)
+    {
+        if (!in.Get((char *)&m_position, sizeof(FilePositionType)))
+            return Bad<SizeType>(*this, 0);
+        m_sync_flags ^= ms_position_sync;
+    }
+    else
+        in.SeekOffset(sizeof(FilePositionType), WayType::current);
+    return Good<SizeType>(*this, sizeof(FilePositionType));
+}
+
+
+bool Tree::operator==(const RecordInterfaceType & rec) const
+{
+    auto * rec_tree = dynamic_cast<const Tree *>(&rec);
+    if (!rec_tree) return false;
+    if (rec_tree->m_position == m_position)
+        return true;
+    if (rec_tree->m_position != -1 ||
+        m_position != -1) return false;
+    return m_pathname.compare(rec_tree->m_pathname) == 0;
+}
+
+bool Tree::operator!=(const RecordInterfaceType & rec) const
+{
+    return !(*this == rec);
 }
