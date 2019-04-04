26de190c3192192c9cdaeebed716890eee72f0a4
Ananta Yudica <nta040689@gmail.com>
2019-04-05 05:41:13 +0700

    Added class for definition pathname key value type

---
+++ include/defn/mgmt/pathname/Key.h
@@ -0,0 +1,21 @@
+#ifndef DEFN_MGMT_PATHNAME_KEY_H_
+#define DEFN_MGMT_PATHNAME_KEY_H_
+
+#include <cstdint>
+
+namespace defn
+{
+namespace mgmt
+{
+namespace pathname
+{
+
+using KeyValue = std::int64_t;
+
+} //!pathname
+
+} //!mgmt
+
+} //!defn
+
+#endif //!DEFN_MGMT_PATHNAME_KEY_H_
