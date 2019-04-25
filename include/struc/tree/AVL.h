af056a89a3c172346b4dd32d5dc73c5f097df84a
Ananta Yudica <nta040689@gmail.com>
2019-04-25 23:05:58 +0700

    Fixed balance instruction Updated member functions instruction Updated type from Pointer of Node to Reference of Node Updated using type trait

--- include/struc/tree/AVL.h
+++ include/struc/tree/AVL.h
@@ -2,7 +2,7 @@
 #define STRUC_TREE_AVL_H_
 
 #include "struc/tree/avl/intf/Node.h"
-#include "struc/tree/avl/intf/Pointer.h"
+#include "struc/tree/avl/intf/Edge.h"
 
 #include <cstdint>
 #include <stack>
@@ -18,255 +18,279 @@ class AVL
 {
 public:
     template<typename TData>
-    using NodePointerType = std::shared_ptr<avl::intf::Node<TData>>;
+    using NodeType = avl::intf::Node<TData>;
 public:
     AVL() = default;
 private:
-    template<typename TData>
-    NodePointerType<TData> RotateRight(NodePointerType<TData> root);
-    template<typename TData>
-    NodePointerType<TData> RotateLeft(NodePointerType<TData> root);
+    template<typename TTrait, typename TData>
+    void RotateRight(NodeType<TData> & root_node);
 private:
-    template<typename TData>
-    NodePointerType<TData> Rebalance(NodePointerType<TData> root);
+    template<typename TTrait, typename TData>
+    void RotateLeft(NodeType<TData> & root_node);
+private:
+    template<typename TTrait, typename TData>
+    void Rebalance(NodeType<TData> & root_node);
 public:
     template<typename TData, typename TCompareFunc>
-    NodePointerType<TData> Find(NodePointerType<TData> root, 
-        const TData & data, TCompareFunc compare_func);
-    template<typename TData, typename TCompareFunc>
-    NodePointerType<TData> Insert(NodePointerType<TData> root, 
-        TData & data, TCompareFunc compare_func);
-private:
-    template<typename TData>
-    NodePointerType<TData> Erase(NodePointerType<TData> root);
-    template<typename TData>
-    NodePointerType<TData> EraseMaximum(NodePointerType<TData> root, 
-        NodePointerType<TData> swap);
-    template<typename TData>
-    NodePointerType<TData> EraseMinimum( NodePointerType<TData> root, 
-         NodePointerType<TData> swap);
+    TData Find(NodeType<TData> & root_node, const TData & data, 
+        TCompareFunc compare_func);
 public:
-    template<typename TData, typename TCompareFunc>
-    NodePointerType<TData> Erase(NodePointerType<TData> root, 
-        const TData & data, TCompareFunc compare_func);
+    template<typename TTrait, typename TData, typename TCompareFunc>
+    void Insert(NodeType<TData> & root_node, TData & data, 
+        TCompareFunc compare_func);
+private:
+    template<typename TTrait, typename TData>
+    void EraseMaximum(NodeType<TData> & root_node, 
+        NodeType<TData> & swap_node);
+private:
+    template<typename TTrait, typename TData>
+    void EraseMinimum(NodeType<TData> & root_node, 
+        NodeType<TData> & swap_node);
+private:
+    template<typename TTrait, typename TData>
+    TData Erase(NodeType<TData> & root_node);
+public:
+    template<typename TTrait, typename TData, typename TCompareFunc>
+    TData Erase(NodeType<TData> & root_node, const TData & data, 
+        TCompareFunc compare_func);
 };
 
-template<typename TData>
-typename AVL::NodePointerType<TData> 
-AVL::RotateRight(NodePointerType<TData> root)
+template<typename TTrait, typename TData>
+void AVL::RotateRight(NodeType<TData> & root_node)
 {
-    if (!root || !*root) return nullptr;
-    auto & root_left = *(root->Left());
-    auto & root_left_right = *(root_left.Right());
+    if (!root_node) return;
 
-    root->Left() = root_left_right;
-    if (root_left_right)
-        root_left_right.Parent() = *root;
-    
-    root_left.Right() = *root;
-    root_left.Parent() = *(root->Parent());
-    root->Parent() = root_left;
+    auto root_left_edge = TTrait::Assign(root_node.Left());
+    auto root_left_right_edge = TTrait::Assign(root_left_edge->Right());
 
-    root->Hight(std::max(root->Left()->Hight(), root->Right()->Hight()) + 1);
-    root_left.Hight(std::max(root_left.Left()->Hight(), 
-        root_left.Right()->Hight()) + 1);
+    TTrait::Assign(root_left_right_edge->Parent(), root_node);
+    TTrait::Assign(root_node.Left(), *root_left_right_edge);
     
-    return std::move(root_left.MakeMove());
+    TTrait::Assign(root_left_edge->Parent(), root_node.Parent());
+
+    TTrait::Assign(root_node.Right(), root_node);
+    TTrait::Assign(root_node.Parent(), *root_left_edge);
+
+    TTrait::Hight(root_node, TTrait::Hight(root_node) + 1);
+    TTrait::Hight(root_left_edge, TTrait::Hight(root_left_edge) + 1);
+    
+    TTrait::Assign(root_node, *root_left_edge);
 }
 
-template<typename TData>
-typename AVL::NodePointerType<TData> 
-AVL::RotateLeft(NodePointerType<TData> root)
+template<typename TTrait, typename TData>
+void AVL::RotateLeft(NodeType<TData> & root_node)
 {
-    if (!root || !*root) return nullptr;
-    auto & root_right = *(root->Right());
-    auto & root_right_left = *(root_right.Left());
+    if (!root_node) return;
 
-    root->Right() = root_right_left;
-    if (root_right_left)
-        root_right_left.Parent() = *root;
-    
-    root_right.Left() = *root;
-    root_right.Parent() = *(root->Parent());
-    root->Parent() = root_right;
+    auto root_right_edge = TTrait::Assign(root_node.Right());
+    auto root_right_left_edge = TTrait::Assign(root_right_edge->Left());
 
-    root->Hight(std::max(root->Left()->Hight(), root->Right()->Hight()) + 1);
-    root_right.Hight(std::max(root_right.Left()->Hight(), 
-        root_right.Right()->Hight()) + 1);
+    TTrait::Assign(root_right_left_edge->Parent(), root_node);
+    TTrait::Assign(root_node.Right(), *root_right_left_edge);
 
-    return std::move(root_right.MakeMove());
+    TTrait::Assign(root_right_edge->Parent(), root_node.Parent());
+  
+    TTrait::Assign(root_right_edge->Left(), root_node);
+    TTrait::Assign(root_node.Parent(), *root_right_edge);
+
+    TTrait::Hight(root_node, TTrait::Hight(root_node) + 1);
+    TTrait::Hight(root_right_edge, TTrait::Hight(root_right_edge) + 1);
+
+    TTrait::Assign(root_node, *root_right_edge);
 }
 
-template<typename TData>
-typename AVL::NodePointerType<TData> 
-AVL::Rebalance(NodePointerType<TData> root)
+template<typename TTrait, typename TData>
+void AVL::Rebalance(NodeType<TData> & root_node)
 {
-    if (!root || !*root) return nullptr;
-    const auto right_balance = root->Right()->Balance();
-    const auto left_balance = root->Left()->Balance();
-    if (right_balance >= 0)
-        return RotateLeft(root);
-    else if(left_balance <= 0)  
-        return RotateRight(root);
-    else if(right_balance == -1)
+    if (!root_node) return;
+    const auto balance = root_node.Balance();
+    if (balance < -1)
     {
-        return RotateLeft(RotateRight(root->Right()->MakeCopy()));
+        const auto left_balance = root_node.Left()->Balance();
+        if(left_balance <= 0)  
+            RotateRight<TTrait>(root_node);
+        else
+        {
+            auto left_node = TTrait::Assign(*(root_node.Left()));
+            RotateLeft<TTrait>(left_node);
+            TTrait::Assign(root_node.Left(), left_node);
+            RotateRight<TTrait>(root_node);
+        }
     }
-    else if(left_balance == 1)
+    else if(balance > 1)
     {
-        return RotateRight(RotateLeft(root->Left()->MakeCopy()));
+        const auto right_balance = root_node.Right()->Balance();
+        if (right_balance >= 0)
+            return RotateLeft<TTrait>(root_node);
+        else
+        {
+            auto right_node = TTrait::Assign(*(root_node.Right()));
+            RotateRight<TTrait>(right_node);
+            TTrait::Assign(root_node.Right(), right_node);
+            RotateLeft<TTrait>(root_node);
+        }
     }
-    return root;
 }
 
 template<typename TData, typename TCompareFunc>
-typename AVL::NodePointerType<TData> 
-AVL::Find(NodePointerType<TData> root, const TData & data, 
+TData AVL::Find(NodeType<TData> & root_node, const TData & data, 
     TCompareFunc compare_func)
 {
-    if (!root || !*root) return nullptr;
-    const auto res = compare_func(**root, data);
+    if (!root_node) return TData();
+    const auto res = compare_func(root_node.Data(), data);
     if (res == 0)
-        return root;
-    else if (res == 1)
-        return Find(root->Right()->MakeCopy(), data, compare_func);
-    return Find(root->Left()->MakeCopy(), data, compare_func);
-}
-
-template<typename TData, typename TCompareFunc>
-typename AVL::NodePointerType<TData> AVL::Insert(NodePointerType<TData> root, 
-    TData & data, TCompareFunc compare_func)
-{
-    if (!root) return nullptr;
-    if (!*root)
+        return root_node.Data();
+    else if (res >= 1)
     {
-        root->Emplace(data);
-        return root;
-    }
-    const auto res = compare_func(**root, data);
-    if (res == 0)
-        return root;
-    else if (res == 1)
-    {
-        if (*(root->Right()))
-            Insert(root->Right()->MakeCopy(), data, compare_func);
+        if (root_node.HasRight())
+            return Find(*(root_node.Right()), data, compare_func);
         else
-            root->Right().Emplace(data);
+            return TData();
     }
-    else
-    {
-        if (*(root->Left()))
-            Insert(root->Left()->MakeCopy(), data, compare_func);
-        else
-            root->Left().Emplace(data);
-    }
-
-    root->Hight(std::max(root->Left()->Hight(), root->Right()->Hight()) + 1);
-    return Rebalance(root);
+    if (root_node.HasLeft())
+        return Find(*(root_node.Left()), data, compare_func);
+    return TData();
 }
 
-template<typename TData>
-typename AVL::NodePointerType<TData> AVL::Erase(NodePointerType<TData> root)
-{
-    const bool has_right = root->Right();
-    const bool has_left = root->Left();
-    if (!has_left && has_right)
-    {
-        if (root->Parent()->Right() == *root)
-            root->Parent()->Right() = *(root->Right());
-        else if (root->Parent()->Left() == *root)
-            root->Parent()->Left() = *(root->Right());
-        root->Right()->Parent() = *(root->Parent());
-    }
-    else if (has_left && !has_right)
-    {
-        if (root->Parent()->Right() == *root)
-            root->Parent()->Right() = *(root->Left());
-        else if (root->Parent()->Left() == *root)
-            root->Parent()->Left() = *(root->Left());
-        root->Left()->Parent() = *(root->Parent());
-    }
-    else if (has_left && has_right)
-    {
-        if (root->Parent()->Right()->Hight() >
-            root->Parent()->Left()->Hight())
-            EraseMaximum(root, root->Left()->MakeCopy());
-        else
-            EraseMinimum(root, root->Right()->MakeCopy());
-    }
-    else
-    {
-        if (root->Parent()->Right() == *root)
-            root->Parent()->Right().Displace();
-        else if (root->Parent()->Left() == *root)
-            root->Parent()->Left().Displace();
-    }
-
-    root->Parent().Displace();
-    return root;
-}
-
-template<typename TData>
-typename AVL::NodePointerType<TData> 
-AVL::EraseMaximum(NodePointerType<TData> root, NodePointerType<TData> swap)
-{
-    if (swap->Right())
-        EraseMaximum(root, swap->Right()->MakeCopy());
-    else
-    {
-        root->Swap(*swap);
-        Erase(swap);
-        swap->Displace();
-        return root;
-    }
-    
-    root->Hight(std::max(root->Left()->Hight(), root->Right()->Hight()) + 1);
-    return Rebalance(root);
-        
-}
-
-template<typename TData>
-typename AVL::NodePointerType<TData> 
-AVL::EraseMinimum(NodePointerType<TData> root, NodePointerType<TData> swap)
-{
-    if (swap->Left())
-        EraseMaximum(root, swap->Left()->MakeCopy());
-    else
-    {
-        root->Swap(*swap);
-        Erase(swap);
-        swap->Displace();
-        return root;
-    }
-    
-    root->Hight(std::max(root->Left()->Hight(), root->Right()->Hight()) + 1);
-    return Rebalance(root);
-}
-
-template<typename TData, typename TCompareFunc>
-typename AVL::NodePointerType<TData>  
-AVL::Erase(NodePointerType<TData> root, const TData & data, 
+template<typename TTrait, typename TData, typename TCompareFunc>
+void AVL::Insert(NodeType<TData> & root_node, TData & data, 
     TCompareFunc compare_func)
 {
-    if (!root || !*root) return nullptr;
-    const auto res = compare_func(**root, data);
-    if (res == 0)
+    if (!root_node) TTrait::Emplace(root_node, data);
+    const auto res = compare_func(root_node.Data(), data);
+    if (res == 0) return;
+    else if (res >= 1)
     {
-        Erase(root);
-    }
-    else if (res == 1)
-    {
-        if (!Erase(root->Right()->MakeCopy(), data, compare_func)) 
-            return root;
+        if (root_node.HasRight())
+        {
+            auto node = TTrait::Assign(*(root_node.Right()));
+            Insert<TTrait>(node, data, compare_func);
+            TTrait::Assign(root_node.Right(), node);
+        }
+        else TTrait::Emplace(root_node.Right(), data);
     }
     else
     {
-        if (!Erase(root->Left()->MakeCopy(), data, compare_func)) 
-            return root;
+        if (root_node.HasLeft())
+        {
+            auto node = TTrait::Assign(*(root_node.Left()));
+            Insert<TTrait>(node, data, compare_func);
+            TTrait::Assign(root_node.Left(), node);
+        }
+        else TTrait::Emplace(root_node.Left(), data);
+    }
+    TTrait::Hight(root_node, TTrait::Hight(root_node) + 1);
+    Rebalance<TTrait>(root_node);
+}
+
+template<typename TTrait, typename TData>
+void AVL::EraseMaximum(NodeType<TData> & root_node, NodeType<TData> & swap_node)
+{
+    if (!root_node || !swap_node) return;
+    if (swap_node.HasRight())
+    {
+        auto swap_right_node = TTrait::Assign(*(swap_node.Right()));
+        EraseMaximum<TTrait>(root_node, swap_right_node);
+        TTrait::Assign(swap_node.Right(), swap_right_node);
+    }
+    else
+    {
+        TTrait::Swap(root_node, swap_node);
+        TTrait::Displace(swap_node);
+        return;
+    }
+    
+    TTrait::Hight(swap_node, TTrait::Hight(swap_node) + 1);
+    Rebalance<TTrait>(swap_node);
+}
+
+template<typename TTrait, typename TData>
+void AVL::EraseMinimum(NodeType<TData> & root_node, 
+    NodeType<TData> & swap_node)
+{
+    if (!root_node || !swap_node) return;
+    if (swap_node.HasLeft())
+    {
+        auto swap_left_node = TTrait::Assign(*(swap_node.Left()));
+        EraseMinimum<TTrait>(root_node, swap_left_node);
+        TTrait::Assign(swap_node.Left(), swap_left_node);
+    }
+    else
+    {
+        TTrait::Swap(root_node, swap_node);
+        TTrait::Displace(swap_node);
+        return;
+    }
+    
+    TTrait::Hight(swap_node, TTrait::Hight(swap_node) + 1);
+    Rebalance<TTrait>(swap_node);
+}
+
+template<typename TTrait, typename TData>
+TData AVL::Erase(NodeType<TData> & root_node)
+{
+    if (!root_node) return TData();
+    TData data = root_node.Data();
+    if (!root_node.HasLeft() && root_node.HasRight())
+    {
+        auto node = TTrait::Assign(root_node);
+        TTrait::Assign(root_node.Right()->Parent(), root_node.Parent());
+        TTrait::Assign(root_node, *(root_node.Right()));
+        TTrait::Displace(node);
+    }
+    else if (root_node.HasLeft() && !root_node.HasRight())
+    {
+        auto node = TTrait::Assign(root_node);
+        TTrait::Assign(root_node.Left()->Parent(), root_node.Parent());
+        TTrait::Assign(root_node, *(root_node.Left()));
+        TTrait::Displace(node);
+    }
+    else if (root_node.HasLeft() && root_node.HasRight())
+    {
+        if (TTrait::Hight(root_node.Right()) > 
+            TTrait::Hight(root_node.Left()))
+        {
+            auto left_node = TTrait::Assign(*(root_node.Left()));
+            EraseMaximum<TTrait>(root_node, left_node);
+        }
+        else
+        {
+            auto right_node = TTrait::Assign(*(root_node.Right()));
+            EraseMinimum<TTrait>(root_node, right_node);
+        }
+    }
+    else TTrait::Displace(root_node);
+    return data;
+}
+
+template<typename TTrait, typename TData, typename TCompareFunc>
+TData AVL::Erase(NodeType<TData> & root_node, const TData & data, 
+    TCompareFunc compare_func)
+{
+    if (!root_node) return TData();
+    const auto res = compare_func(root_node.Data(), data);
+    TData ret_data;
+    if (res == 0)
+        ret_data = Erase<TTrait>(root_node);
+    else if (res == 1)
+    {
+        auto right_node = TTrait::Assign(*(root_node.Right()));
+        ret_data = Erase<TTrait>(right_node, data, compare_func);
+        TTrait::Assign(root_node.Right(), right_node);
+    }
+    else
+    {
+        auto left_node = TTrait::Assign(*(root_node.Left()));
+        ret_data = Erase<TTrait>(left_node, data, compare_func);
+        TTrait::Assign(root_node.Left(), left_node);
     }
    
-    root->Hight(std::max(root->Left()->Hight(), root->Right()->Hight()) + 1);
-    return Rebalance(root);
+    if (root_node)
+    {
+        TTrait::Hight(root_node, TTrait::Hight(root_node) + 1);
+        Rebalance<TTrait>(root_node);
+    }
+    return ret_data;
 }
 
 } //!tree
